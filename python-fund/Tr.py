0c009cbbb9a8d7db368ee7c502b77c6b39ae1991
Fatima-Zahra2004 <aboutalbi.fatimazahra2004@gmail.com>
2024-11-28 00:32:43 +0100

    Modification du code

---
+++ python-fund/Tr.py
@@ -0,0 +1,16 @@
+
+# print("Form Loop")
+
+# x ="*"
+# for i in range (0,11):
+#     print (x * i)
+    
+##################################################################
+
+        # While Loop
+x = 0
+y = 40
+while x < y:
+    print (x + 1)
+    x = x+1
+
